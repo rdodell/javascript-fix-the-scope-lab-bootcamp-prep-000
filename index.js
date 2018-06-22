ebe2fed762f13b0c1638288cc7932cdbe34af092
Rachel Dodell <rachel@racheldodell.com>
2018-06-22 22:09:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ function myAnimal() {
   return animal
 }
 
-animal='cat'
-
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  animal='cat'
   return animal
 }
 
